41ab2c603a50768c02f4279ef556bfae84bbfeeb
hiimanshu28 <himanshu.soni71@gmail.com>
2016-10-09 12:02:58 +0000

    Added form create action and validations

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            flash[:success]="Message has been sent."
+            redirect_to new_contact_path
+        else
+            flash[:danger]="Message hasn't been sent! The name and email fields can't be left blank."
+            redirect_to new_contact_path
+        end
     end
+    
+    private
+        
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
